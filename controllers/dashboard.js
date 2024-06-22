4326bec7935f6d539f1708e99c42544ad1568e49
pawan-artistonk <pawan.artistonk@gmail.com>
2024-06-22 10:21:30 +0530

    Stats Updated

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const Mail = require("../models/Email");
 const Sender = require("../models/Sender");
 const Recipient = require("../models/Recipient");
+const RecipientGroup = require("../models/RecipientGroup");
 
 exports.getStats = async (req, res) => {
   try {
     const totalMails = await Mail.countDocuments();
     const totalSenders = await Sender.countDocuments();
     const totalRecipients = await Recipient.countDocuments();
+    const totalRecipientsGroup = await RecipientGroup.countDocuments();
 
     res.json({
       totalMails,
       totalSenders,
       totalRecipients,
+      totalRecipientsGroup,
     });
   } catch (err) {
     console.error(err.message);
